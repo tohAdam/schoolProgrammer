59d18d911f6d2be518d73b5df066aa4d760d7455
tohAdam <139485068+tohAdam@users.noreply.github.com>
2023-12-13 23:24:09 +0800

    loop

---
+++ problem D.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    
+    for(a = 1; a <= 5; a++)
+    {
+        for (b = 1; b <= a; b++)
+            printf("%d ", b);
+        printf ("\n") ;
+    }
+    return 0;
+}
